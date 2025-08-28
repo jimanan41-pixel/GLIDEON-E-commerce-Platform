71fafa910d608db059458cdeaca590f5c3e46abc
jimanan41-pixel <jimanan41@gmail.com>
2025-08-29 01:35:57 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+// import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { CartProvider } from "@/contexts/CartContext";
 import { useAuth } from "@/hooks/useAuth";
@@ -84,10 +84,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <CartProvider>
-          <TooltipProvider>
+          <div>
             <Toaster />
             <Router />
-          </TooltipProvider>
+          </div>
         </CartProvider>
       </ThemeProvider>
     </QueryClientProvider>
